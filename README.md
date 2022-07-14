07e2091229f7157a771ff0da5ff285b4c79a7750
LuanSantos00 <luancosmefilho@gmail.com>
2022-07-14 08:38:41 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# ShopAPI
+
+
+Iniciando os estudos do CQRS e MEDIATOR com dotnet.
+
+<hr>
+
+Starting CQRS and MEDIATOR studies with dotnet.
---
+++ Shop/Domain/Handlers/CreateCustomerHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Shop.Domain.Commands.Requests;
+using Shop.Domain.Commands.Responses;
+
+namespace Shop.Domain.Handlers
+{
+    public class CreateCustomerHandler : IRequestHandler<CreateCustomerRequest, CreateCustomerResponse>
+    {
+
+        public Task<CreateCustomerResponse> Handle(CreateCustomerRequest request, CancellationToken cancellationToken)
+        {
+            // Verifica se o cliente já está cadastrado
+            // Valida os dados
+            // Insere o cliente
+            // Envia E-mail de boas vindas
+
+
+            var result = new CreateCustomerResponse
+            {
+                Id = Guid.NewGuid(),
+                Name = "Luan Santos",
+                Email = "luancosmefilho@gmail.com",
+                Date = DateTime.Now,
+            };
+
+            return Task.FromResult(result);
+        }
+    }
+}
